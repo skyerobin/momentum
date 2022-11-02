5295821242873393508f3183c1795f7eda347b29
00cccc <117288816+00cccc@users.noreply.github.com>
2022-11-03 00:36:19 +0900

    Add files via upload

---
+++ search_bar.js
@@ -0,0 +1,46 @@
+const Searching = Search.prototype;
+  
+  function Search(){
+      this.keyword = document.querySelector('input[name = "search"]');
+      this.engine = document.querySelector('.SelectSearch');
+      this.button = document.querySelector('.img-button');
+      this.form = document.querySelector('.search');
+  
+      this.Engine();
+  }
+  
+  Searching.Engine = function(){
+      this.form.addEventListener('submit', e => {
+          
+          e.preventDefault();
+  
+  
+          let engine = this.engine.value;
+          let keyword = this.keyword.value;
+          if(engine === 'google'){
+              location.href = 'https://www.google.co.kr/search?q=' + keyword;
+          }else if(engine === 'naver'){
+              location.href = 'https://search.naver.com/search.naver?query=' + keyword;
+          }else if(engine === 'youtube'){
+            location.href = 'https://www.youtube.com/results?search_query=' + keyword;
+          }else if(engine === 'Navershopping'){
+            location.href = 'https://search.shopping.naver.com/search/all?query=' + keyword;
+          }else if(engine === 'kyobo'){
+            location.href ='https://me2.kr/' +keyword;
+          }else if(engine === 'instagram'){
+            location.href ='https://www.instagram.com/explore/tags/' + keyword;
+          }else if(engine === 'twitter'){
+            location.href ='https://twitter.com/search?q=' + keyword;  
+          }else if(engine === 'music'){
+              location.href = 'https://music.youtube.com/search?q=' + keyword;
+          }else if(engine === 'nintendo'){
+              location.href = 'https://www.nintendo.co.kr/search.php?globalSearch='+keyword;
+          }else if(engine ==='naveren'){
+              location.href = 'https://en.dict.naver.com/#/search?range=all&query=' +keyword;
+          }else{
+            location.href = 'https://ja.dict.naver.com/?s=h#/search?query=' + keyword;
+          }
+      });
+  }
+  
+  new Search();
