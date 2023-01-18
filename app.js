5250a3b440d7cb5c64bdc85251d94f1097d253ca
00cccc <117288816+00cccc@users.noreply.github.com>
2023-01-18 17:48:11 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,28 +1,138 @@
-const loginForm = document.querySelector("#login-form");
-const loginInput = document.querySelector("#login-form input");
-const greeting = document.querySelector("#greeting");
+const saveBtn= document.getElementById("save")
+const textInput = document.getElementById("text");
+const fileInput = document.getElementById("file");
+const modeBtn = document.getElementById("mode-btn")
+const destroyBtn = document.getElementById("destroy-btn")
+const eraserBtn = document.getElementById("eraser-btn");
+const colorOptions = Array.from(
+  document.getElementsByClassName("color-option")
+);
+const color = document.getElementById("color");
+const lineWidth = document.getElementById("line-width");
+const canvas = document.querySelector("canvas")
+const ctx = canvas.getContext("2d")
 
-const HIDDEN_CLASSNAME = "hidden";
-const USERNAME_KEY = "username";
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 800;
 
-function onLoginSubmit(event) {
-  event.preventDefault();
-  loginForm.classList.add(HIDDEN_CLASSNAME);
-  const username = loginInput.value;
-  localStorage.setItem(USERNAME_KEY, username);
-  paintGreetings(username);
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
+ctx.lineWidth = lineWidth.value;
+ctx.lineCap = "round";
+let isPainting = false;
+let isFilling = false;
+  
+function onMove(event) {
+    if (isPainting) {
+      ctx.lineTo(event.offsetX, event.offsetY);
+      ctx.stroke();
+      return;
+    }
+    ctx.beginPath();
+    ctx.moveTo(event.offsetX, event.offsetY);
+  }
+  function startPainting() {
+    isPainting = true;
+  }
+  function cancelPainting() {
+    isPainting = false;
+  }
+  
+  function onLineWithChange(event){
+    console.log(event.target.value);
+    ctx.lineWidth = event.target.value;
+  }
+
+  function onColorChange(event){
+    ctx.strokeStyle = event.target.value;
+    ctx.fillStyle = event.target.value;
+  }
+
+
+  function onColorClick(event) {
+    const colorValue = event.target.dataset.color;
+    ctx.strokeStyle = colorValue;
+    ctx.fillStyle = colorValue;
+    color.value = colorValue;
+  }
+
+  function onModeClick(){
+    if(isFilling){
+      isFilling = false
+      modeBtn.innerText = "Fill";
+    } else {
+      isFilling = true
+      modeBtn.innerText = "Draw";
+    }
+  }
+
+  function onCanvasClick(){
+    if(isFilling){
+      ctx.fillRect(0,0, CANVAS_WIDTH, CANVAS_HEIGHT)
+    }
+  }
+
+  function onDestoryClick(){
+    ctx.fillStyle = "white";
+    ctx.fillRect(0,0, CANVAS_WIDTH, CANVAS_HEIGHT)
+  }
+
+
+  function onEraserClick(){
+    ctx.strokeStyle = "white";
+    isFilling = false;
+    modeBtn.innerText = "Fill";
+
+  }
+
+  function onFileChange(event){
+    const file = event.target.files[0];
+    const url = URL.createObjectURL(file)
+    const image = new Image()
+    image.src = url;
+    image.onload = function(){
+      ctx.drawImage(image,0, 0, CANVAS_WIDTH, CANVAS_WIDTH);
+      fileInput.value = null;
+    }
+
+  }
+
+  function onDoublClick(event){
+    const text = textInput.value;
+    if(text !== ""){
+    ctx.save();
+    ctx.lineWidth= 1;
+    ctx.font = "68px serif"
+    ctx.fillText(text, event.offsetX, event.offsetY);
+    ctx.restore();
+  }  
+} 
+
+function onSaveClick(){
+  const url = canvas.toDataURL();
+  const a = document.createElement("a")
+  a.href =url
+  a.download = "myDrawing.pnd"
+  a.click();
+  
 }
 
-function paintGreetings(username) {
-  greeting.innerText = `Hello ${username}`;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
-}
 
-const savedUsername = localStorage.getItem(USERNAME_KEY);
 
-if (savedUsername === null) {
-  loginForm.classList.remove(HIDDEN_CLASSNAME);
-  loginForm.addEventListener("submit", onLoginSubmit);
-} else {
-  paintGreetings(savedUsername);
-}
+canvas.addEventListener("dblclick", onDoublClick);
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting);
+canvas.addEventListener("click", onCanvasClick);
+lineWidth.addEventListener("change", onLineWithChange)
+color.addEventListener("change", onColorChange)
+
+colorOptions.forEach(color => color.addEventListener("click",
+onColorClick))
+
+modeBtn.addEventListener("click", onModeClick)
+destroyBtn.addEventListener("click", onDestoryClick);
+eraserBtn.addEventListener("click", onEraserClick);
+fileInput.addEventListener("change", onFileChange);
+saveBtn.addEventListener("click", onSaveClick);
